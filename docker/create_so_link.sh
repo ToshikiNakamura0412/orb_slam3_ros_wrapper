03378b19a39b8bec1512741957e06e321e02d972
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-11-15 21:05:32 +0900

    Update

--- docker/create_so_link.sh
+++ docker/create_so_link.sh
@@ -2,11 +2,9 @@ ln -s /home/amsl/Dev/Pangolin/build/libpango_display.so /opt/ros/noetic/lib/libp
 ln -s /home/amsl/Dev/Pangolin/build/libpango_opengl.so /opt/ros/noetic/lib/libpango_opengl.so
 ln -s /home/amsl/Dev/Pangolin/build/libpango_vars.so /opt/ros/noetic/lib/libpango_vars.so
 ln -s /home/amsl/Dev/Pangolin/build/libpango_core.so /opt/ros/noetic/lib/libpango_core.so
-# ln -s /home/amsl/Dev/Pangolin/build/ /opt/ros/noetic/lib/
 
 ln -s /home/amsl/Dev/opencv/build/lib/libopencv_highgui.so.4.4 /opt/ros/noetic/lib/libopencv_highgui.so.4.4
 ln -s /home/amsl/Dev/opencv/build/lib/libopencv_calib3d.so.4.4 /opt/ros/noetic/lib/libopencv_calib3d.so.4.4
 ln -s /home/amsl/Dev/opencv/build/lib/libopencv_features2d.so.4.4 /opt/ros/noetic/lib/libopencv_features2d.so.4.4
 ln -s /home/amsl/Dev/opencv/build/lib/libopencv_imgproc.so.4.4 /opt/ros/noetic/lib/libopencv_imgproc.so.4.4
 ln -s /home/amsl/Dev/opencv/build/lib/libopencv_core.so.4.4 /opt/ros/noetic/lib/libopencv_core.so.4.4
-# ln -s /home/amsl/Dev/opencv/build/lib/ /opt/ros/noetic/lib/
